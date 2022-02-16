d6db50630cdf818a35c718344f7d83d2a1656995
Robbie K <rkahl08@gmail.com>
2022-02-15 19:20:31 -0600

    code updates for JS

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,20 @@
-window.alert("This is an Alert. JavaScript is running!");
+var playerName = window.prompt("what is your robot's name");
+
+// what is this
+
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message")
+
+//this will do match and log 20
+console.log(10 + 10);
+//what is this? 
+console.log("Our robot's name is " + playerName);
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun");
+}
+
+//fight();
